d10aea2a058be56e0cfe8dd839e0030915611fa8
Antti Leppä <antti.leppa@metatavu.fi>
2016-12-05 08:18:08 +0200

    Added support for using CKEditor as a content editor instead of tinymce

---
+++ ckeditor.php
@@ -0,0 +1,14 @@
+
+<?php
+  defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
+  
+  if (is_admin()) {
+  	add_action('init', function() {
+  	  add_filter('ckeditor_external_plugins', function ($plugins) {
+  	  	$plugins['kunta-api-aside'] = plugin_dir_url(__FILE__) . 'ckeditor-plugins/kunta-api-aside/plugin.js';
+  	  	return $plugins;
+  	  });
+    });
+  	
+  }
+?>
