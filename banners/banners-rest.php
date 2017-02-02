26d27616d7f9ba5ee537a341b0cb4012ed469dbc
Antti Leppä <antti.leppa@metatavu.fi>
2017-02-02 18:47:23 +0200

    Published banner colors and hiding from REST

--- banners/banners-rest.php
+++ banners/banners-rest.php
@@ -12,20 +12,57 @@
     class BannersRest {
       
       public function __construct() {
-        register_rest_field( 'banner', 'banner-link', array(
-          'get_callback' => 'getBannerField',
+        register_rest_field( 'banner', 'banner-link', [
+          'get_callback' => [$this, 'getBannerMeta'],
           'update_callback' => null,
-          'schema' => array (
+          'schema' => [
             "type" => "string",
         	"format" => "url",
         	"description" => "Banner link"
-          )
-        ));
+          ]
+        ]);
+        
+        register_rest_field( 'banner', 'banner-text-color', [
+          'get_callback' => [$this, 'getBannerMeta'],
+          'update_callback' => null,
+          'schema' => [
+        	"type" => "string",
+        	"description" => "Banner text color"
+          ] 
+        ]);
+        
+        register_rest_field( 'banner', 'banner-background-color', [
+          'get_callback' => [$this, 'getBannerMeta'],
+          'update_callback' => null,
+          'schema' => [
+        	"type" => "string",
+        	"description" => "Banner background color"
+          ] 
+        ]);
+        
+        register_rest_field( 'banner', 'banner-hide-title', [
+          'get_callback' => [$this, 'getBannerMetaBoolean'],
+          'update_callback' => null,
+          'schema' => [
+        	"type" => "boolean",
+        	"description" => "Banner hide title"
+          ] 
+        ]);
+      }
+
+      public function getBannerMeta($object, $field_name) {
+      	$value = get_post_meta($object[ 'id' ], $field_name, true);
+      	if ($value) {
+      	  return $value;
+      	}
+      	
+      	return null;
+      }
+
+      public function getBannerMetaBoolean($object, $field_name) {
+      	return "true" == $this->getBannerMeta($object, $field_name);
       }
       
-      public function getBannerField($object, $field_name) {
-      	return get_post_meta($object[ 'id' ], $field_name, true);
-      }
     }
   
   }
