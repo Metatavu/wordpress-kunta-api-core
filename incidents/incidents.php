09ebc0c03facc896f032551bc77d669bb1c32d36
Antti Leppä <antti.leppa@metatavu.fi>
2017-06-12 06:35:16 +0300

    Added select all / select none link into incident areas select

--- incidents/incidents.php
+++ incidents/incidents.php
@@ -8,4 +8,8 @@ require_once( __DIR__ . '/incidents-settings.php');
 require_once( __DIR__ . '/incidents-taxonomy.php');
 require_once( __DIR__ . '/acf.php');
 
+add_action('admin_enqueue_scripts', function () {
+  wp_enqueue_script('incident-acf-js', plugin_dir_url(__FILE__) . 'acf.js', array(), KUNTA_API_CORE_PLUGIN_VERSION, true);
+});
+
 ?>
