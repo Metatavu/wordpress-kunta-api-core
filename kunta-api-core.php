0b0f66891e1ae816fe9f7797587d46b513a28907
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-07-10 10:03:58 +0300

    Update kunta-api-core.php

--- kunta-api-core.php
+++ kunta-api-core.php
@@ -3,7 +3,7 @@
  * Created on Oct 21, 2016
  * Plugin Name: Kunta API Core
  * Description: Core functionalities for Kunta API integrations
- * Version: 0.7.4
+ * Version: 0.7.5
  * Author: Metatavu Oy
  * Text Domain: kunta_api_core
  */
@@ -15,7 +15,7 @@
   }
   
   if (!defined('KUNTA_API_CORE_PLUGIN_VERSION')) {
-    define('KUNTA_API_CORE_PLUGIN_VERSION', '0.7.4');
+    define('KUNTA_API_CORE_PLUGIN_VERSION', '0.7.5');
   }
   
   require_once( __DIR__ . '/updates.php');
