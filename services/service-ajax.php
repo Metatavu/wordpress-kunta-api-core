ebd408157b3c11d11fd3600e18e349197428aa1d
Antti Leppä <antti.leppa@metatavu.fi>
2017-12-16 16:19:10 +0200

    Added support for load and save service ajax calls

--- services/service-ajax.php
+++ services/service-ajax.php
@@ -21,6 +21,20 @@
     wp_die();
   } );
   
+  add_action( 'wp_ajax_kunta_api_load_service', function () {
+    try {
+      $serviceId = $_POST['serviceId'];
+      $service = \KuntaAPI\Services\Loader::findService($serviceId);
+      $json = $service->__toString();
+      echo $json;
+      wp_die();
+    } catch (\KuntaAPI\ApiException $e) {
+      $message = json_encode($e->getResponseBody());
+      wp_die($message, null, [
+        response => $e->getCode()
+      ]);
+    }
+  });
   
   add_action( 'wp_ajax_kunta_api_render_service_component', function () {
     $renderer = new \KuntaAPI\Services\ServiceComponentRenderer();
@@ -37,7 +51,21 @@
     echo '</article>';
     wp_die();
   } );
-
-
-
+  
+  add_action( 'wp_ajax_kunta_api_save_service', function () {
+    try {
+      $data = stripslashes($_POST['service']);
+      $service = new \KuntaAPI\Model\Service(json_decode($data, true));
+      $result = \KuntaAPI\Core\Api::getServicesApi()->updateService($service->getId(), $service);
+      $json = $result->__toString();
+      echo $json;
+      wp_die();
+    } catch (\KuntaAPI\ApiException $e) {
+      $message = json_encode($e->getResponseBody());
+      wp_die($message, null, [
+        response => $e->getCode()
+      ]);
+    }
+  });
+  
 ?>
