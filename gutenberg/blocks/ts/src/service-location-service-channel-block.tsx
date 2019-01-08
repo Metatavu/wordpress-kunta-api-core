da42cfbaac48f58216e4c0a66df35577d30bdb66
Antti Leppä <antti.leppa@metatavu.fi>
2019-01-08 06:38:21 +0200

    Moved service location service block into it's own component

--- gutenberg/blocks/ts/src/service-location-service-channel-block.tsx
+++ gutenberg/blocks/ts/src/service-location-service-channel-block.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { wp } from 'wp';
-import { SearchModal } from './search-modal';
-import ServiceLocationPageCheckbox from './service-location-page-checkbox';
+import ServiceLocationServiceBlock from './service-location-service-block';
 
 declare var wp: wp;
 const { __ } = wp.i18n;
@@ -32,83 +31,25 @@ wp.blocks.registerBlockType('kunta-api/service-location-service-channel', {
   /**
    * Block type edit method 
    */
-  edit: wp.compose.withState({ isOpen: false, channelId: null, serviceLocationPage: false })((params: any) => {
-    const Button = wp.components.Button;
-    const attributes = params.attributes;
-    const components = [
-      "description",
-      "adresses",
-      "email",
-      "fax",
-      "name",
-      "phone",
-      "phone-charge-info",
-      "servicehours",
-      "webpages"
-    ];
-    
-    const languages = ["fi", "sv", "en"];
-
-    const componetOptions = components.map((component) => {
-      return { label: __(`servicelocationservicechannel.${component}`, 'kunta_api_core'), value: component };
-    });
-
-    const languageOptions = languages.map((language) => {
-      return { label: __(`language.${language}`, 'kunta_api_core'), value: language };
-    });
-    
+  edit: ((params: any) => {
     return (
-      <div>
-        <div>
-          <div style={{ float: "right" }}>
-            <Button className="button" isDefault onClick={ () => params.setState( { isOpen: true } ) }>{__( 'Change service location', 'kunta_api_core' )}</Button>
-          </div> 
-          <div style={{ fontSize: "16px"}}>
-          <div style={{ float: "left", paddingRight: "5px" }}>{__( 'Current service location:', 'kunta_api_core' )}</div> 
-            <wp.components.ServerSideRender block="kunta-api/service-location-service-channel" attributes={attributes} urlQueryArgs={{displayName: true}} />
-          </div>
-        </div>
-
-        <wp.components.SelectControl 
-          label={ __("Component", 'kunta_api_core') }
-          value={ attributes.component } 
-          options={ componetOptions } 
-          onChange={ ( component: any ) => { params.setAttributes({"component": component}) }} />
-
-        <wp.components.SelectControl 
-          label={ __("Language", 'kunta_api_core') } 
-          value={ attributes.lang } 
-          options={ languageOptions } 
-          onChange={ ( lang: any ) => { params.setAttributes({"lang": lang}) }} />
-
-        <SearchModal 
-          modalTitle={ __("Search Services Locations", 'kunta_api_core') }
-          inputLabel={ __("Search Services Locations", "kunta_api_core") }
-          inputHelp={ __("Enter some text to search service locations", "kunta_api_core") }
-          searchAction="kunta_api_search_service_location_channels"
-          open={ params.isOpen }
-          getDisplayName={ (entity: any) => {
-            const names = entity.names || [];
-            names.sort((a: any, b: any) => {
-              return a.language === 'fi' ? -1 : 1;
-            });
-          
-            return names.length ? names[0].value : null;
-          }}
-          onSelect={ (data) => { 
-            params.setState( { isOpen: false, channelId: data.id } ); 
-            params.setAttributes({"channelId": data.id}); 
-          } }
-          onClose={ () => params.setState( { isOpen: false } )}/> 
-
-        <ServiceLocationPageCheckbox 
-          channelId={ params.channelId || attributes.channelId } 
-          onChange={ (isChecked: boolean) => { console.log("isChecked", isChecked); params.setAttributes({"serviceLocationPage": isChecked}) } }/>
-        
-        <hr/>
-        
-        <wp.components.ServerSideRender block="kunta-api/service-location-service-channel" attributes={attributes} urlQueryArgs={{preview: true}} />
-      </div>
+      <ServiceLocationServiceBlock 
+        component={params.attributes.component}
+        lang={params.attributes.lang}
+        channelId={params.attributes.channelId}
+        serviceLocationPage={params.attributes.serviceLocationPage}
+        onComponentChange={(component: string) => {
+          params.setAttributes({"component": component});
+        }}
+        onLangChange={(lang: string) => {
+          params.setAttributes({"lang": lang});
+        }}
+        onChannelIdChange={(channelId: string) => {
+          params.setAttributes({"channelId": channelId});
+        }}
+        onServiceLocationPageChange={(serviceLocationPage: boolean) => {
+          params.setAttributes({"serviceLocationPage": serviceLocationPage});
+        }}/>
     );
   }),
   
