070405c893ca080210259ef642e4343c9de3ebbd
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2019-02-18 12:59:47 +0200

    Fixed acf compability issues

--- shortlinks/acf.php
+++ shortlinks/acf.php
@@ -1,7 +1,7 @@
 <?php
-if(function_exists("register_field_group"))
+if(function_exists("acf_add_local_field_group"))
 {
-	register_field_group(array (
+	acf_add_local_field_group(array (
 		'id' => 'acf_shortlinks',
 		'title' => 'Shortlinks',
 		'fields' => array (
